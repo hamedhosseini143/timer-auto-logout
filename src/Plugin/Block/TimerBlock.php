e7bdf726d4b65b8d894b27b989a563e29bdf3182
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-09-15 15:52:58 +0330

    add config field to block form

--- src/Plugin/Block/TimerBlock.php
+++ src/Plugin/Block/TimerBlock.php
@@ -8,6 +8,7 @@ use Drupal\autologout\AutologoutManagerInterface;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a timer auto logout block.
@@ -53,10 +54,53 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
   /**
    * @return array
    */
+  public function defaultConfiguration(): array {
+    return [
+      'prefix_text' => $this->t('Hello world!'),
+      'suffix_text' => $this->t('Goodbye world!'),
+    ];
+  }
+
+  /**
+   * @param $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return array
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form['prefix_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('prefix text'),
+      '#default_value' => $this->configuration['prefix_text'],
+    ];
+    $form['suffix_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('suffix text'),
+      '#default_value' => $this->configuration['suffix_text'],
+    ];
+    return $form;
+  }
+
+  /**
+   * @param $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return void
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    $this->configuration['prefix_text'] = $form_state->getValue('prefix_text');
+    $this->configuration['suffix_text'] = $form_state->getValue('suffix_text');
+  }
+
+  /**
+   * @return array
+   */
   public function build(): array {
     $remainingTime = $this->autoLogoutManager->createTimer();
     $build['content'] = [
       '#remainingTime' => $remainingTime,
+      '#prefix_text' => $this->configuration['prefix_text'],
+      '#suffix_text' => $this->configuration['suffix_text'],
       '#theme' => 'timer_autoLogout',
       '#attached' => [
         'library' => [
