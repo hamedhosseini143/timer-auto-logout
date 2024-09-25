d08e473adcc4428e83b155669120dad1e522b7ca
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-09-25 16:25:14 +0330

    fix js code and overwrite my js code

---
+++ dist/js/timer_auto_logout.timer.js
@@ -0,0 +1,74 @@
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.timerAutoLogout = {
+    attach: function (context, settings) {
+      // Start the timer
+      const intervalValue = +document.querySelector('.interval_value').innerHTML;
+      let dateNow = new Date();
+      dateNow.setSeconds(dateNow.getSeconds() + intervalValue);
+
+      // Store the timestamp in localStorage
+      localStorage.setItem('timer_auto_logout_reset-timer', dateNow);
+
+      let isAjaxRequestSent = false; // Flag to prevent multiple requests
+
+      // Format seconds to HH:MM:SS
+      function formatSeconds(seconds) {
+        const hrs = Math.floor(seconds / 3600);
+        const mins = Math.floor((seconds % 3600) / 60);
+        const secs = seconds % 60;
+
+        const formattedHrs = String(hrs).padStart(2, '0');
+        const formattedMins = String(mins).padStart(2, '0');
+        const formattedSecs = String(secs).padStart(2, '0');
+
+        return `${formattedHrs}:${formattedMins}:${formattedSecs}`;
+      }
+
+      // Function to check time and update interval
+      function checkTime() {
+        const timestamp = new Date(localStorage.getItem('timer_auto_logout_reset-timer'));
+
+        const interval = setInterval(() => {
+          const updateDate = new Date();
+          let newTimestamp = new Date(localStorage.getItem('timer_auto_logout_reset-timer'));
+          const newTime = Math.floor((newTimestamp - updateDate) / 1000);
+
+          // Update time display
+          document.querySelector('.interval-set').innerText = formatSeconds(newTime);
+
+          // Handle click event for resetting the timer
+          $('#timer_auto_logout_reset-timer', context).on('click', function () {
+            const updateTime = new Date();
+
+            if (!isAjaxRequestSent) {
+              // Check if the request is already sent
+              isAjaxRequestSent = true; // Mark the request as sent
+              $.ajax({
+                url: '/autologout_ajax_set_last', type: 'GET', success: function (data) {
+                  isAjaxRequestSent = false; // Reset the flag after successful request
+                }, error: function () {
+                  isAjaxRequestSent = false; // Reset the flag on error
+                },
+              });
+
+              // Extend the timestamp on reset
+              newTimestamp = new Date(updateTime.setSeconds(updateTime.getSeconds() + intervalValue));
+              localStorage.setItem('timer_auto_logout_reset-timer', newTimestamp);
+            }
+          });
+          if (newTime === 0) {
+            clearInterval(interval);
+            setTimeout(() => {
+              window.location.href = '/user/login';
+            }, 5000);
+          }
+
+        }, 1000);
+      }
+
+      checkTime();
+
+      // End the timer
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
--- src/Plugin/Block/TimerBlock.php
+++ src/Plugin/Block/TimerBlock.php
@@ -2,19 +2,21 @@
 
 declare(strict_types=1);
 
-namespace Drupal\timer_autologout\Plugin\Block;
+namespace Drupal\timer_auto_logout\Plugin\Block;
 
 use Drupal\autologout\AutologoutManagerInterface;
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a timer auto logout block.
  *
  * @Block(
- *   id = "timer_autologout_timer",
+ *   id = "timer_auto_logout",
  *   admin_label = @Translation("timer auto logout"),
  *   category = @Translation("Custom"),
  * )
@@ -58,7 +60,6 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
     return [
       'prefix_text' => $this->t('Hello world!'),
       'suffix_text' => $this->t('Goodbye world!'),
-      'modal_timer' => 10,
     ];
   }
 
@@ -79,39 +80,56 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
       '#title' => $this->t('suffix text'),
       '#default_value' => $this->configuration['suffix_text'],
     ];
-   $form['modal_timer'] =[
-      '#type' => 'textfield',
-      '#title' => $this->t('Time for modal'),
-      '#default_value' => $this->configuration['modal_timer'],
-    ];
     return $form;
   }
 
-
+  /**
+   * @param $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return void
+   */
   public function blockSubmit($form, FormStateInterface $form_state): void {
     $this->configuration['prefix_text'] = $form_state->getValue('prefix_text');
     $this->configuration['suffix_text'] = $form_state->getValue('suffix_text');
-    $this->configuration['modal_timer'] = $form_state->getValue('modal_timer');
   }
 
-
+  /**
+   * @return array
+   */
   public function build(): array {
     $remainingTime = $this->autoLogoutManager->createTimer();
+//    $paddingTime = $this->autoLogoutManager->
+    //TODO: get padding time from autologout manager
     $build['content'] = [
       '#remainingTime' => $remainingTime,
       '#prefix_text' => $this->configuration['prefix_text'],
       '#suffix_text' => $this->configuration['suffix_text'],
-      '#modal_timer' => $this->configuration['modal_timer'],
-      '#theme' => 'timer_autoLogout',
+      '#theme' => 'timer_auto_logout',
       '#attached' => [
         'library' => [
-          'timer_autologout/timer_auto_logout',
+          'timer_auto_logout/timer_auto_logout',
         ],
       ],
+    ];
+    $build['reset_timer'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Reset Timer'),
+      '#attributes' => ['id' => 'timer_auto_logout_reset-timer'],
     ];
 
     return $build;
 
+  }
+
+  /**
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   */
+  protected function blockAccess(AccountInterface $account): AccessResult {
+    // Only authenticated users can see this block.
+    return AccessResult::allowedIf($account->isAuthenticated());
   }
 
 }
