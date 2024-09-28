2004753d5ec0122b18f2c51361f0372a6baa7a7d
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-09-28 15:15:57 +0330

    add padding time

--- src/Plugin/Block/TimerBlock.php
+++ src/Plugin/Block/TimerBlock.php
@@ -7,6 +7,7 @@ namespace Drupal\timer_auto_logout\Plugin\Block;
 use Drupal\autologout\AutologoutManagerInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -31,14 +32,23 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
   protected AutologoutManagerInterface $autoLogoutManager;
 
   /**
+   * The Config service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected ConfigFactory $config;
+
+  /**
    * @param array $configuration
    * @param $plugin_id
    * @param $plugin_definition
    * @param \Drupal\autologout\AutologoutManagerInterface $autoLogout
+   * @param \Drupal\Core\Config\ConfigFactory $config
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, AutologoutManagerInterface $autoLogout) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AutologoutManagerInterface $autoLogout, ConfigFactory $config) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->autoLogoutManager = $autoLogout;
+    $this->config = $config;
   }
 
   /**
@@ -50,7 +60,7 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
    * @return self
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
-    return new self($configuration, $plugin_id, $plugin_definition, $container->get('autologout.manager'));
+    return new self($configuration, $plugin_id, $plugin_definition, $container->get('autologout.manager'), $container->get('config.factory'));
   }
 
   /**
@@ -99,12 +109,14 @@ final class TimerBlock extends BlockBase implements ContainerFactoryPluginInterf
    */
   public function build(): array {
     $remainingTime = $this->autoLogoutManager->createTimer();
-//    $paddingTime = $this->autoLogoutManager->
-    //TODO: get padding time from autologout manager
+    $getUserRedirectUrl = $this->autoLogoutManager->getUserRedirectUrl();
+    $TimeoutPadding = $this->config->get('autologout.settings')->get('padding');
     $build['content'] = [
       '#remainingTime' => $remainingTime,
       '#prefix_text' => $this->configuration['prefix_text'],
       '#suffix_text' => $this->configuration['suffix_text'],
+      '#getUserRedirectUrl' => $getUserRedirectUrl,
+      '#timeoutPadding' => $TimeoutPadding,
       '#theme' => 'timer_auto_logout',
       '#attached' => [
         'library' => [
